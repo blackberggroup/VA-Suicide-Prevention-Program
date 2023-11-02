84e9b09bf7ff3cf9071ac13443bb2c7c8a1470b3
will-bbg <will.elder@blackberggroup.com>
2023-11-02 06:33:36 -0500

    adds build files & bootstrap

---
+++ build/config.js
@@ -0,0 +1,10 @@
+const path = {
+  html: ['./dist', './docs', './components'],
+  scss: 'src/scss',
+  src_js: 'src/js',
+  js: 'dist/assets/js',
+  css: 'dist/assets/css',
+  vendor: 'dist/assets/vendor',
+}
+
+module.exports = path
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+/**
+ * Suicide Prevention Program | VA OMHSP
+ * Copyright 2023 Blackberg Group
+ * Website core scripts
+ *
+ * @author Blackberg Group
+ * @version 1.0
+ */
+
+// Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+// Smooth scroll
+import 'smooth-scroll/dist/smooth-scroll.polyfills'
+
+import './components/smooth-scroll'
+import './components/scroll-top-button'
+import './components/tooltip'
+import './components/popover'
